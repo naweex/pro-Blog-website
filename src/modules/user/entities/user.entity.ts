de5ab49426f3be14dc7913d7fac23ef88b4d96c9
navid bahrami <navidbahrami1996@gmail.com>
2025-01-23 19:32:18 +0330

    user entity

---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { Col } from "reactstrap";
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntitiName } from "src/common/enums/entity.enum";
+import { Column, Entity } from "typeorm";
+//we create a func in common/enums and define user and use it here in @entity.
+//BaseEntity is a custome function we created.
+@Entity(EntitiName.User)
+export class UserEntity extends BaseEntity{//we dont use id because in baseEntity that we extend on we define id.
+    @Column({unique : true})//username in not nullable because when user create an account by default create a unique username.
+    username : string;
+    @Column({unique : true , nullable : true})
+    phone : string;
+    @Column({unique : true , nullable : true})
+    email : string;
+}
