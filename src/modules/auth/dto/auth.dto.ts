f512a1fb3ecfd7a76fdafcd5651c882a529cf16c
navid bahrami <navidbahrami1996@gmail.com>
2025-01-25 10:55:13 +0330

    auth dto

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { AuthType } from "../enums/type.enum";
+import { IsEnum, IsString, Length } from "class-validator";
+import { AuthMethod } from "../enums/method.enum";
+
+export class AuthDto {
+    @ApiProperty()//with this decorator we can show variable in below in swagger.
+    @IsString()
+    @Length(3 , 100)
+    username : string;
+    @ApiProperty({enum : AuthType})
+    @IsEnum(AuthType)
+    type : string;
+    @ApiProperty({enum : AuthMethod})
+    @IsEnum(AuthMethod)
+    method : AuthMethod;
+}
+
+//enum means specific type or sum ,when our type is enum our variable always give specific amount.
