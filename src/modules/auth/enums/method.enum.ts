3a94e512b7f558df75e19d7a53f99e55e21e26fd
navid bahrami <navidbahrami1996@gmail.com>
2025-01-25 10:54:49 +0330

    method enum

---
+++ src/modules/auth/enums/method.enum.ts
@@ -0,0 +1,6 @@
+export enum AuthMethod {
+    Username = 'username' ,
+    Phone = 'phone' ,
+    Email = 'email' 
+}
+//enum means specific type or sum ,when our type is enum our variable always give specific amount.
