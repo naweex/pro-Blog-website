b0e5decd3e855cbc7a0bbf4c0abc1d1988a26f00
navid bahrami <navidbahrami1996@gmail.com>
2025-01-25 17:24:19 +0330

    auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,12 +7,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '../user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileEntity } from '../user/entities/profile.entity';
+import { OtpEntity } from '../user/entities/otp.entity';
+import { randomInt } from 'crypto';
 
 @Injectable()
 export class AuthService {
     constructor(
       @InjectRepository(UserEntity) private userRepository:Repository<UserEntity>,
-      @InjectRepository(ProfileEntity) private profileRepository:Repository<ProfileEntity>
+      @InjectRepository(ProfileEntity) private profileRepository:Repository<ProfileEntity> ,
+      @InjectRepository(OtpEntity) private otpRepository:Repository<OtpEntity> ,
+
     ){}
       UserExistence(authDto : AuthDto){
         const {method , type , username} = authDto;
@@ -29,13 +33,52 @@ export class AuthService {
         const validUsername = this.usernameValidator(method , username);
         let user : UserEntity = await this.checkExistUser(method , validUsername)
           if(!user) throw new UnauthorizedException('cant found any account')
+        const otp = await this.saveOtp(user.id)
+        return {
+          code : otp.code 
+        }
       }
      async register(method : AuthMethod , username : string){
         const validUsername = this.usernameValidator(method , username)
         let user : UserEntity = await this.checkExistUser(method , validUsername)
           if(user) throw new ConflictException('this account already exist')
+          if(method === AuthMethod.Username) throw new BadRequestException('invalid data for register')
+        user = this.userRepository.create({
+          [method] : username 
+        })
+        user = await this.userRepository.save(user);
+        user.username = `m_${user.id}` //create unique userID for users that register their account.
+        await this.userRepository.save(user);
+        const otp = await this.saveOtp(user.id);
+        return {
+          code : otp.code 
+        }
       }
       async checkOtp(){}
+      async saveOtp(userId : number){
+        const code = randomInt(10000 , 99999).toString();
+        const expiresIn = new Date(Date.now() + (1000 * 60 * 2))
+        let otp = await this.otpRepository.findOneBy({userId});
+        let existOtp = false;
+        if(otp){
+          existOtp = true
+          otp.code = code;
+          otp.expiresIn = expiresIn;
+        }else {
+            otp = this.otpRepository.create({
+                code ,
+                expiresIn ,
+                userId
+            })
+        }
+        otp = await this.otpRepository.save(otp)
+        if(!existOtp){
+          await this.userRepository.update({id : userId} , {
+          otpId : otp.id
+        })
+        }
+        return otp;
+      }
       async checkExistUser(method : AuthMethod , username : string){
         let user : UserEntity;
         if(method === AuthMethod.Phone){
