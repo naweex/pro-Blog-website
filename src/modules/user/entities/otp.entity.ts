377f795185151807dd72aa76366e0493c8bc8b1a
navid bahrami <navidbahrami1996@gmail.com>
2025-01-25 17:23:51 +0330

    otp entity

---
+++ src/modules/user/entities/otp.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntitiName } from "src/common/enums/entity.enum";
+import { Column, Entity, OneToOne } from "typeorm";
+import { UserEntity } from "./user.entity";
+
+@Entity(EntitiName.Otp)
+export class OtpEntity extends BaseEntity {
+    @Column()
+    code : string
+    @Column()
+    expiresIn : Date
+    @Column()
+    userId : number
+    @OneToOne(() => UserEntity , user => user.otp , {onDelete : 'CASCADE'}) //every user had one otp => one to one relation.
+    user : UserEntity
+}
