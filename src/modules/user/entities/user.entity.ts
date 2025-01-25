8786e807fc259648f40a2310f21f062364f32bdc
navid bahrami <navidbahrami1996@gmail.com>
2025-01-25 17:23:37 +0330

    user entity

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,12 +1,13 @@
 import { Col } from "reactstrap";
 import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntitiName } from "src/common/enums/entity.enum";
-import { Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, UpdateDateColumn } from "typeorm";
+import { OtpEntity } from "./otp.entity";
 //we create a func in common/enums and define user and use it here in @entity.
 //BaseEntity is a custome function we created.
 @Entity(EntitiName.User)
 export class UserEntity extends BaseEntity{//we dont use id because in baseEntity that we extend on we define id.
-    @Column({unique : true})//username in not nullable because when user create an account by default create a unique username.
+    @Column({unique : true , nullable : true})
     username : string;
     @Column({unique : true , nullable : true})
     phone : string;
@@ -14,6 +15,11 @@ export class UserEntity extends BaseEntity{//we dont use id because in baseEntit
     email : string;
     @Column({nullable : true})
     password : string
+    @Column({nullable : true})
+    otpId : number
+    @OneToOne(() => OtpEntity , otp => otp.user , {nullable : true})
+    @JoinColumn()
+    otp : OtpEntity
     @CreateDateColumn()
     created_at : Date
     @UpdateDateColumn()
