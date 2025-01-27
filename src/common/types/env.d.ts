fe419c79bf3ea8136ae3b793c520b06227881090
navid bahrami <navidbahrami1996@gmail.com>
2025-01-27 17:13:28 +0330

    env config

---
+++ src/common/types/env.d.ts
@@ -0,0 +1,16 @@
+//we develope ProccessEnv interface in nodejs namespace.
+namespace NodeJS {
+    interface ProcessEnv {
+        //Application
+        PORT:number 
+        //Database
+        DB_PORT:number
+        DB_NAME:string
+        DB_USERNAME:string
+        DB_PASSWORD:string
+        DB_HOST:string
+        //secret
+        COOKIE_SECRET:string
+        OTP_TOKEN_SECRET: string
+    }
+}
