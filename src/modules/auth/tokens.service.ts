74c0d13332b6e95521e7b1505fef877f388add10
navid bahrami <navidbahrami1996@gmail.com>
2025-01-28 12:01:16 +0330

    token service

--- src/modules/auth/tokens.service.ts
+++ src/modules/auth/tokens.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { CookiePayload } from "./types/payload";
-
+import { AccessTokenPayload, CookiePayload } from "./types/payload";
+//*** we create and then verify our otpToken whith JwtService.****
 @Injectable()
 export class TokenService {
     constructor(private jwtService : JwtService){}
@@ -15,9 +15,28 @@ export class TokenService {
     }
     verifyOtpToken(token : string) : CookiePayload {
         try {
-            
+            return this.jwtService.verify(token , {
+                secret : process.env.OTP_TOKEN_SECRET ,
+            })
         } catch (error) {
             throw new UnauthorizedException('please try again')
+        }
+    }
+    //**in access token we can set long time expires but in otp we should set very short time expires.**
+    createAccessToken(payload : AccessTokenPayload){
+        const token = this.jwtService.sign(payload , {
+            secret : process.env.ACCESS_TOKEN_SECRET ,
+            expiresIn : "1y"
+        });
+        return token;
+    }
+    verifyAccessToken(token : string) : AccessTokenPayload {
+        try {
+            return this.jwtService.verify(token , {
+                secret : process.env.ACCESS_TOKEN_SECRET ,
+            })
+        } catch (error) {
+            throw new UnauthorizedException('please try to login again')
         }
     }
     }
