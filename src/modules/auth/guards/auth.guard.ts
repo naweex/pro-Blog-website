2e88bf150404a2943b4cf5c8fef19c023054c3e8
navid bahrami <navidbahrami1996@gmail.com>
2025-01-28 17:42:54 +0330

    auth guard

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -9,17 +9,20 @@ export class AuthGuard implements CanActivate {
     constructor(private authService : AuthService){}
     async canActivate(context: ExecutionContext){
         const httpContext = context.switchToHttp();
-        const request : Request = httpContext.getNext<Request>();
+        const request : Request = httpContext.getRequest<Request>();
+        const token = this.extractToken(request)
         request.user = await this.authService.validateAccessToken(token)
         return true;
     }
+    //Private methods/members are accessible only from inside the class.
+    //Protected methods/members are accessible from inside the class and extending class as well.
     protected extractToken(request : Request){
         const { authorization } = request.headers;
         if(!authorization || authorization?.trim() == ""){
             throw new UnauthorizedException('please login to your current account')
         }
         const [bearer , token] = authorization?.split(" ")
-        if(bearer?.toLowerCase() !== 'beare' || !token || isJWT(token)){
+        if(bearer?.toLowerCase() !== 'bearer' || !token || !isJWT(token)){
             throw new UnauthorizedException('please login to your current account')
         }
         return token;
