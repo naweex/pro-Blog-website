bbe2b8dc4e37347cc55402b3fd4d23c2c3056419
navid bahrami <navidbahrami1996@gmail.com>
2025-01-28 16:55:39 +0330

    swagger config

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,12 +1,22 @@
 import { INestApplication } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SecuritySchemeObject, SecuritySchemeType } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
 //this is swagger config and use this function in main.ts and config it for all project.
 export function swaggerConfigInit(app : INestApplication) : void {
     const document = new DocumentBuilder()
     .setTitle('Blog website')
     .setDescription('Blog website Back-End')
     .setVersion('v0.0.1')
+    .addBearerAuth(swaggerAuthConfig())
     .build()
     const swaggerDocument = SwaggerModule.createDocument(app , document);
     SwaggerModule.setup('/swagger' ,app , swaggerDocument)
+}
+function swaggerAuthConfig() : SecuritySchemeObject {
+    return {
+        type : 'http' ,
+        bearerFormat : 'JWT' , 
+        in : 'header' ,
+        scheme : 'bearer'
+    }
 }
