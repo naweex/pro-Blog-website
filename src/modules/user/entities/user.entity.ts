995cc27460e6b3f9550d7e79c62f4397edae8016
navid bahrami <navidbahrami1996@gmail.com>
2025-01-31 17:48:46 +0330

    add new entities

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntitiName } from "src/common/enums/entity.enum";
 import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, UpdateDateColumn } from "typeorm";
 import { OtpEntity } from "./otp.entity";
+import { ProfileEntity } from "./profile.entity";
 //we create a func in common/enums and define user and use it here in @entity.
 //BaseEntity is a custome function we created.
 @Entity(EntitiName.User)
@@ -14,12 +15,23 @@ export class UserEntity extends BaseEntity{//we dont use id because in baseEntit
     @Column({unique : true , nullable : true})
     email : string;
     @Column({nullable : true})
+    new_email : string;
+    @Column({nullable : true , default : false})
+    verify_email : boolean;
+    @Column({nullable : true , default : false})
+    verify_phone : boolean;
+    @Column({nullable : true})
     password : string
     @Column({nullable : true})
     otpId : number
+    @Column({nullable : true})
+    profileId : number
     @OneToOne(() => OtpEntity , otp => otp.user , {nullable : true})
     @JoinColumn()
     otp : OtpEntity
+    @OneToOne(() => ProfileEntity , profile => profile.user , {nullable : true})
+    @JoinColumn()
+    profile : ProfileEntity
     @CreateDateColumn()
     created_at : Date
     @UpdateDateColumn()
