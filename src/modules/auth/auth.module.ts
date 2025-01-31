53511a6e9907ade60d4222a513bd2266a9827f4a
navid bahrami <navidbahrami1996@gmail.com>
2025-01-31 17:49:25 +0330

    auth modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UserModule } from '../user/user.module';
 import { JwtService } from '@nestjs/jwt';
 import { TokenService } from './tokens.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { OtpEntity } from '../user/entities/otp.entity';
+import { ProfileEntity } from '../user/entities/profile.entity';
 
 @Module({
-  imports : [UserModule] ,
+  imports: [TypeOrmModule.forFeature([UserEntity , OtpEntity , ProfileEntity])] ,//these are users dependencies.
   controllers: [AuthController],
   providers: [AuthService , JwtService , TokenService],
+  exports: [AuthService , JwtService , TokenService , TypeOrmModule],
 })
 export class AuthModule {}
