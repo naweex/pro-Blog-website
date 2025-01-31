89bd58d20f9bcb88fa5d16797ba6320fcfda4a49
navid bahrami <navidbahrami1996@gmail.com>
2025-01-31 12:38:43 +0330

    main

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { swaggerConfigInit } from './config/swagger.config';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   swaggerConfigInit(app);
+  app.useStaticAssets('public')
+  app.useGlobalPipes(new ValidationPipe())//because validation of dtos work.
   app.use(cookieParser(process.env.COOKIE_SECRET))
   const {PORT} = process.env;
   await app.listen(PORT , ()=> {
