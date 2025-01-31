9fa47eafb220344a397e6c8b045869a412e1b155
navid bahrami <navidbahrami1996@gmail.com>
2025-01-31 17:57:09 +0330

    env types

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -13,5 +13,9 @@ namespace NodeJS {
         COOKIE_SECRET:string
         OTP_TOKEN_SECRET: string
         ACCESS_TOKEN_SECRET: string
+        EMAIL_TOKEN_SECRET: string
+        PHONE_TOKEN_SECRET: string
     }
 }
+//WE IMPROVE OUR APPLICATION SECURITY WITH SEPARATE access tokens,
+//one type code for phone verification and another for email.
