31ce80d47281e67471d72a9b4fb7c5531eaa257a
navid bahrami <navidbahrami1996@gmail.com>
2025-01-31 12:35:51 +0330

    user service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -20,18 +20,18 @@ export class UserService {
   async changeProfile(files : ProfileImage , profileDto: ProfileDto) {
     if(files?.image_profile?.length > 0 ){
       let [image] = files?.image_profile;
-      profileDto.image_profile = image.path;
+      profileDto.image_profile = image?.path?.slice(7);//because /public is 7 character. 
     }
     if(files?.backGround_profile?.length > 0 ){
       let [image] = files?.backGround_profile;
-      profileDto.backGround_profile = image.path;
-      
+      profileDto.backGround_profile = image?.path?.slice(7);//because /public is 7 character. 
     }
     const {id : userId , profileId} = this.request.user; //give userid in request.user
     let profile = await this.profileRepository.findOneBy({userId});
     const {bio , birthday , gender , linkedin_profile , nick_name , x_profile , image_profile , backGround_profile} = profileDto;
     if(profile){
         if(bio) profile.bio = bio;
+        if(nick_name) profile.nick_name = nick_name;
         if(birthday && isDate(new Date(birthday))) profile.birthday = new Date(birthday);
         //object.value treat with value like an object(Gender has 3 value and object.value read them)
         if(gender && Object.values(Gender as any).includes(gender)) profile.gender = gender;
@@ -59,7 +59,16 @@ export class UserService {
         await this.userRepository.update({id : userId} , {profileId : profile.id})
         //if user doesnt have profileid updated it.
     }
-
+    return {
+      message : 'successfully updated'
+    }
+  }
+  profile(){
+    const {id} = this.request.user
+      return this.userRepository.findOne({
+        where : {id} ,
+        relations : ['profile']
+      })
   }
 
   findAll() {
