4da7ff7c26f671dc69e06e70403938f3cadbab85
navid bahrami <navidbahrami1996@gmail.com>
2025-02-03 11:16:55 +0330

    blog entity

--- src/modules/blog/entities/blog.entity.ts
+++ src/modules/blog/entities/blog.entity.ts
@@ -1,6 +1,11 @@
 import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntitiName } from "src/common/enums/entity.enum";
-import { Column, CreateDateColumn, Entity, UpdateDateColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, UpdateDateColumn } from "typeorm";
+import { BlogStatus } from "../enum/status.enum";
+import { UserEntity } from "src/modules/user/entities/user.entity";
+import { BlogLikesEntity } from "./like.entity";
+import { BlogBookmarkEntity } from "./bookmark.entity";
+import { BlogCommentEntity } from "./comment.entity";
 
 @Entity(EntitiName.Blog)
 export class BlogEntity extends BaseEntity {//in base entity only id exist.
@@ -10,12 +15,20 @@ export class BlogEntity extends BaseEntity {//in base entity only id exist.
     description : string; //brief description about user blog**
     @Column()
     content : string; //content and users story(blog)main blog**
-    @Column()
+    @Column({nullable : true})//nullable : true it meant the parameter optional and it could be null.
     image : string;//image of blog,,every blogs that user create can have image that is optional.
-    @Column()
+    @Column({default : BlogStatus.Draft}) //by default in first step when user create blog it is draft.
     status : string
     @Column()
-    authorId : number;
+    authorId : number;//author and blog had many to one relationship/one author can have many blogs.
+    @ManyToOne(() => UserEntity , user => user.blog , {onDelete : 'CASCADE'})//ondelete : cascade mean if author deleted every blogs with that author deleted.
+    author : UserEntity;
+    @OneToMany(() => BlogLikesEntity , like => like.blog)
+    likes : BlogLikesEntity[] 
+    @OneToMany(() => BlogBookmarkEntity , bookmark => bookmark.blog)
+    bookmarks : BlogBookmarkEntity[] //a blog can bookmarked by several users.
+    @OneToMany(() => BlogCommentEntity , comment => comment.blog)
+    comments : BlogCommentEntity[]
     @CreateDateColumn()
     created_at : Date; //time of creating blog
     @UpdateDateColumn()
