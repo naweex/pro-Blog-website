49dafcd8e201b3aa72660a842a72a7703ea25311
navid bahrami <navidbahrami1996@gmail.com>
2025-02-04 15:20:29 +0330

    blog contrillers

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
+import { CreateBlogDto } from './dto/blog.dto';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Controller('blog')
+@ApiTags('Blog')
+@ApiBearerAuth('Authorization')
+@UseGuards(AuthGuard)
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
+
+
+  @Post('/')
+  @ApiConsumes(SwaggerConsumes.UrlEncoded , SwaggerConsumes.Json)
+  create(@Body() blogDto : CreateBlogDto){
+    return this.blogService.create(blogDto)
+  }
+
+
 }
