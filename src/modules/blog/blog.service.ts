2da52ae6fc37219e54b4bbcca93ff7204b906720
navid bahrami <navidbahrami1996@gmail.com>
2025-02-04 15:20:05 +0330

    blog service

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { BlogEntity } from './entities/blog.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateBlogDto } from './dto/blog.dto';
 
 @Injectable()
-export class BlogService {}
+
+export class BlogService {
+    constructor(
+        @InjectRepository(BlogEntity) private blogRepository : Repository<BlogEntity> ,
+    ){}
+
+
+    create(blogDto : CreateBlogDto){
+        let {title , slug , content , description , image , time_for_study} = blogDto;
+        let slugData = slug ?? title;                                        //slugify dont soppurt persian and arabic we should use regexp.**
+        slug = slugData?.replace(/[ًٌٍَُِّ\.\+\-_)(*&%$^#@!~'";:?><`ء)]+/g , '')?.replace(/[\s]+/g,'-')//regexp = where exist 1 or more than one space replace them with "-".
+        return blogDto;
+    }
+}
