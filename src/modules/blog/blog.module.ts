d19524732951d0e88bee2149f642dc3a1a0506a8
navid bahrami <navidbahrami1996@gmail.com>
2025-02-05 22:51:30 +0330

    blog module

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -4,10 +4,13 @@ import { BlogController } from './blog.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './entities/blog.entity';
+import { CategoryService } from '../category/category.service';
+import { CategoryEntity } from '../category/entities/category.entity';
+import { BlogCategoryEntity } from './entities/blog-category.entity';
 
 @Module({
-  imports : [AuthModule , TypeOrmModule.forFeature([BlogEntity])] ,
+  imports : [AuthModule , TypeOrmModule.forFeature([BlogEntity , CategoryEntity , BlogCategoryEntity])] ,
   controllers: [BlogController],
-  providers: [BlogService],
+  providers: [BlogService , CategoryService],
 })
 export class BlogModule {}
