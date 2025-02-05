87edd13a9d50819c29946b255d4ddcdd75ba6f2a
navid bahrami <navidbahrami1996@gmail.com>
2025-02-05 16:15:29 +0330

    category entity

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,11 +1,14 @@
 import { BaseEntity } from "src/common/abstracts/base.entity";
 import { EntitiName } from "src/common/enums/entity.enum";
-import { Column, Entity } from "typeorm";
+import { BlogCategoryEntity } from "src/modules/blog/entities/blog-category.entity";
+import { Column, Entity, OneToMany } from "typeorm";
 
 @Entity(EntitiName.Category)//EntitiName file we created in enums to nomination our entities.
 export class CategoryEntity extends BaseEntity { //BaseEntity file we created and we write id and dont need every time in entities repeat id.
     @Column()
-    title : string
+    title : string;
     @Column({nullable : true})
-    priority : number
+    priority : number;
+    @OneToMany(() => BlogCategoryEntity , blog => blog.category)
+    blog_categories : BlogCategoryEntity[]
 }
