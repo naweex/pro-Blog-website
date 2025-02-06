05b49fdb083f7586a79f60208a310fe8133f6e23
navid bahrami <navidbahrami1996@gmail.com>
2025-02-06 15:58:51 +0330

    add delete route

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDto, FilterBlogDto } from './dto/blog.dto';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
@@ -33,5 +33,9 @@ export class BlogController {
   @FilterBlog()
   find(@Query() paginationDto : PaginationDto , @Query() filterDto : FilterBlogDto){
     return this.blogService.blogList(paginationDto , filterDto)
+  } 
+  @Delete('/:id')
+  delete(@Param('id' , ParseIntPipe) id : number ){
+    return this.blogService.delete(id)
   } 
 }
