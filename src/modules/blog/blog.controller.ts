e32a90a1ed3c20df8732cd44b3ae254ba1466b2a
navid bahrami <navidbahrami1996@gmail.com>
2025-02-07 12:44:01 +0330

    add new route

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { BlogService } from './blog.service';
-import { CreateBlogDto, FilterBlogDto } from './dto/blog.dto';
+import { CreateBlogDto, FilterBlogDto, UpdateBlogDto } from './dto/blog.dto';
 import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -38,4 +38,9 @@ export class BlogController {
   delete(@Param('id' , ParseIntPipe) id : number ){
     return this.blogService.delete(id)
   } 
+  @Put('/:id')
+  @ApiConsumes(SwaggerConsumes.UrlEncoded , SwaggerConsumes.Json)
+  update(@Param('id' , ParseIntPipe) id : number , @Body() blogDto : UpdateBlogDto){
+    return this.blogService.update(id , blogDto)
+  }
 }
