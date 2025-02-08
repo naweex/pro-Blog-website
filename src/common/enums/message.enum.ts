8840ba8794fcb753ba27269c81af0c93b51ebec9
navid bahrami <navidbahrami1996@gmail.com>
2025-02-08 19:13:42 +0330

    add new message

--- src/common/enums/message.enum.ts
+++ src/common/enums/message.enum.ts
@@ -1,4 +1,6 @@
 export enum PublicMessage {
     Like = 'blog liked successfully' ,
     DisLike = 'blog disliked successfully' ,
+    Bookmark = 'blog bookmarked successfully' ,
+    UnBookmark = 'blog removed from Bookmark list' ,
 }
