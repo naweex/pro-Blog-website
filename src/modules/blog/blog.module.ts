e6b3e31e39268d184ae99b4cb03ff5cb9352451d
navid bahrami <navidbahrami1996@gmail.com>
2025-02-08 19:13:24 +0330

    add new module

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { BlogService } from './blog.service';
-import { BlogController } from './blog.controller';
+import { BlogService } from './services/blog.service';
+import { BlogController } from './controllers/blog.controller';
 import { AuthModule } from '../auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogEntity } from './entities/blog.entity';
@@ -8,15 +8,23 @@ import { CategoryService } from '../category/category.service';
 import { CategoryEntity } from '../category/entities/category.entity';
 import { BlogCategoryEntity } from './entities/blog-category.entity';
 import { BlogLikesEntity } from './entities/like.entity';
+import { BlogBookmarkEntity } from './entities/bookmark.entity';
+import { BlogCommentService } from './services/comment.service';
+import { BlogCommentEntity } from './entities/comment.entity';
 
 @Module({
-  imports : [AuthModule , TypeOrmModule.forFeature([
-  BlogEntity ,
-  CategoryEntity ,
-  BlogCategoryEntity ,
-  BlogLikesEntity ,
-])] ,
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([
+      BlogEntity ,
+      CategoryEntity ,
+      BlogCategoryEntity ,
+      BlogLikesEntity ,
+      BlogBookmarkEntity ,
+      BlogCommentEntity ,
+    ]),
+  ],
   controllers: [BlogController],
-  providers: [BlogService , CategoryService],
+  providers: [BlogService, CategoryService , BlogCommentService],
 })
 export class BlogModule {}
