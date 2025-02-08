6465e8281327ea423280d9112ccc06ebac043dea
navid bahrami <navidbahrami1996@gmail.com>
2025-02-08 22:05:04 +0330

    add create comment

--- src/modules/blog/services/comment.service.ts
+++ src/modules/blog/services/comment.service.ts
@@ -1,34 +1,42 @@
-import {
-    Inject,
-    Injectable,
-    Scope,
-  } from '@nestjs/common';
-  import { BlogEntity } from '../entities/blog.entity';
-  import { Repository } from 'typeorm';
-  import { InjectRepository } from '@nestjs/typeorm';
-  import { REQUEST } from '@nestjs/core';
-  import { Request } from 'express';
-  import { CategoryService } from '../../category/category.service';
-  import { BlogCategoryEntity } from '../entities/blog-category.entity';
-  import { BlogLikesEntity } from '../entities/like.entity';
-  import { BlogBookmarkEntity } from '../entities/bookmark.entity';
-  import { CreateCommentDto } from '../dto/comment.dto';
+import { Inject, Injectable, Scope } from '@nestjs/common';
+import { BlogEntity } from '../entities/blog.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import { CreateCommentDto } from '../dto/comment.dto';
 import { BlogCommentEntity } from '../entities/comment.entity';
 import { BlogService } from './blog.service';
-  
-  @Injectable({ scope: Scope.REQUEST })
-  export class BlogCommentService {
-    constructor(
-      @InjectRepository(BlogEntity)
-      private blogRepository: Repository<BlogEntity>,
-      @InjectRepository(BlogCommentEntity)
-      private blogCommentRepository: Repository<BlogCommentEntity>,
-      @Inject(REQUEST) private request: Request, //LAST Request belong to express.
-      private blogService: BlogService
-    ) {}
-  
-    async createComment(commentDto: CreateCommentDto) {
-      const {parentId , text} = commentDto;
+
+@Injectable({ scope: Scope.REQUEST })
+export class BlogCommentService {
+  constructor(
+    @InjectRepository(BlogEntity)
+    private blogRepository: Repository<BlogEntity>,
+    @InjectRepository(BlogCommentEntity)
+    private blogCommentRepository: Repository<BlogCommentEntity>,
+    @Inject(REQUEST) private request: Request, //LAST Request belong to express.
+    private blogService: BlogService
+  ) {}
+
+  async create(commentDto: CreateCommentDto) {
+    const { parentId, text, blogId } = commentDto;
+    const { id: userId } = this.request.user; //in request we access that user who registered and login.***
+    const blog = await this.blogService.checkExistBlogById(blogId);
+    let parent = null;
+    if (parentId && !isNaN(parentId)) {
+      parent = await this.blogCommentRepository.findOneBy({ id: +parentId });
     }
+    await this.blogCommentRepository.insert({
+      //we use insert instead create because we dont use in the continuation.
+      text,
+      accepted: true,
+      blogId,
+      parentId: parent ? parentId : null,
+      userId,
+    });
+    return {
+      message: 'comment submitted successfully',
+    };
   }
-  
+}
