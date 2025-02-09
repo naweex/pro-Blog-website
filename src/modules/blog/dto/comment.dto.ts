79d1b6037f85b3cc1e4704bcce6da1fe79c245ba
navid bahrami <navidbahrami1996@gmail.com>
2025-02-09 16:06:19 +0330

    bug fixed

--- src/modules/blog/dto/comment.dto.ts
+++ src/modules/blog/dto/comment.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsNumberString, isNumberString, IsOptional, Length } from "class-validator";
+import { IsNumber, isNumber, IsNumberString, isNumberString, IsOptional, Length } from "class-validator";
 
 export class CreateCommentDto {
     @ApiProperty()
     @Length(5)
     text : string
+    @ApiProperty()
+    @IsNumberString()
+    blogId : number
     @ApiPropertyOptional()
     @IsOptional()
     @IsNumberString()
