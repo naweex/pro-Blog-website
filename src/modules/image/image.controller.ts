62b5db08df99767a7285d5966a28052286e4b420
navid bahrami <navidbahrami1996@gmail.com>
2025-02-10 20:08:44 +0330

    add image post

--- src/modules/image/image.controller.ts
+++ src/modules/image/image.controller.ts
@@ -6,17 +6,30 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { ImageService } from './image.service';
 import { ImageDto } from './dto/image.dto';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { ApiBearerAuth, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { UploadFile } from 'src/common/interceptor/upload.interceptor';
+import { MulterFile } from '../user/types/files';
+import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
 
 @Controller('image')
+@ApiTags('image')
+@ApiBearerAuth('Authorization')//for add authentication option in swagger.and should import this in image module.
+@UseGuards(AuthGuard)
 export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Post()
-  create(@Body() imageDto: ImageDto) {
-    return this.imageService.create(imageDto);
+  @UseInterceptors(UploadFile('image'))
+  @ApiConsumes(SwaggerConsumes.MultipartData)
+  create(@Body() imageDto: ImageDto , @UploadedFile() image : MulterFile) {
+    return this.imageService.create(imageDto , image);
   }
 
   @Get()
