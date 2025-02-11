2ac4b9c6aa4430f847efe0ec5621bd0387b62841
navid bahrami <navidbahrami1996@gmail.com>
2025-02-11 22:15:32 +0330

    http module

---
+++ src/modules/http/http.module.ts
@@ -0,0 +1,16 @@
+import { HttpModule } from "@nestjs/axios";
+import { Global, Module } from "@nestjs/common";
+import { timeout } from "rxjs";
+import { KavenegarService } from "./kavenegar.service";
+@Global()//this module able to use all over the project.
+@Module({
+    imports : [HttpModule.register({
+        timeout : 10000 , //if 10 second past from request failed it.//axios confique.
+    })] ,
+    providers : [KavenegarService] ,
+    exports : [KavenegarService] ,
+})
+export class CustomHttpModule {
+
+}
+//add http module into app.module(main module)
