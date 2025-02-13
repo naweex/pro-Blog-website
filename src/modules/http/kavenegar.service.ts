40c992127c1962ea4b80ac0566d1da1580304950
navid bahrami <navidbahrami1996@gmail.com>
2025-02-13 17:10:41 +0330

    add sms service(kavenegar) configuration

--- src/modules/http/kavenegar.service.ts
+++ src/modules/http/kavenegar.service.ts
@@ -1,9 +1,35 @@
 import { HttpService } from "@nestjs/axios";
-import { Injectable } from "@nestjs/common";
+import { Injectable, InternalServerErrorException } from "@nestjs/common";
+import { catchError, lastValueFrom, map } from "rxjs";
+import * as querystring from 'qs'
+import { SmsTemplate } from "./enum/sms-template.enum";
 @Injectable()
 export class KavenegarService {
 constructor(private httpService : HttpService){}
-    async sendVerificationSms(params : string) {
+    async sendVerificationSms(receptor : string , code : string) {
+        const params = querystring.stringify({
+        receptor ,
+        code ,
+        template : SmsTemplate.Verify
+    })   //example:https://api.kavenegar.com/v1/{API-KEY}/verify/lookup.json?receptor=09*********&token=852596&template=myverification
+        console.log(params);
         
+        const {SEND_SMS_URL} = process.env                       //smsURL?params=jakhfuehfd
+        const result = await lastValueFrom(//lastvaluefrom take last value from http request.
+            this.httpService.get(`${SEND_SMS_URL}?${params}`)
+            .pipe(
+                map(res => res.data)
+            )
+            .pipe(//with catcherror we can access to errors in axios***
+                catchError(err => {
+                    console.log(err);
+                    throw new InternalServerErrorException('kavenegar')
+                    
+                })
+            )
+        );
+        console.log(result);
+        return result
+         
     }
 }
