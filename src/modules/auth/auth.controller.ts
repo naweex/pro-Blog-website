2ae6fe84c8e4c92a8e8392e30fecef66d42bce62
navid bahrami <navidbahrami1996@gmail.com>
2025-02-14 17:12:17 +0330

    bug fixed

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,6 +5,9 @@ import { AuthDto, CheckOtpDto } from './dto/auth.dto';
 import { SwaggerConsumes } from 'src/common/enums/swagger-consumes.enum';
 import { Request, Response } from 'express';
 import { AuthGuard } from './guards/auth.guard';
+import { CanAccess } from 'src/common/decorators/role.decorator';
+import { Roles } from 'src/common/enums/role.enum';
+import { AuthDecorator } from 'src/common/decorators/auth.decorator';
 
 
 @Controller('auth')
@@ -22,8 +25,8 @@ export class AuthController {
     return this.authService.checkOtp(checkOtpDto.code)
   }
   @Get('check-login')
-  @ApiBearerAuth('Authorization')//for add authentication option in swagger in check-login.
-  @UseGuards(AuthGuard)
+  @AuthDecorator()                    //can access when we only give admin permission worked else user.
+  @CanAccess(Roles.Admin , Roles.User)//admin and user can access this route.
   checkLogin(@Req() req : Request){
     return req.user;
   }
