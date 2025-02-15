f6bb62014e2b43d32c17c84273c8cfd1caadb933
navid bahrami <navidbahrami1996@gmail.com>
2025-02-15 18:55:50 +0330

    add new module

--- src/common/types/request.d.ts
+++ src/common/types/request.d.ts
@@ -2,8 +2,13 @@ import { UserEntity } from "src/modules/user/entities/user.entity";
 
 declare global {
     namespace Express {
-        interface Request {
+        export interface Request {
             user?: UserEntity
         }
+    }
+}
+declare module "express-serve-static-core" {
+    export interface Request {
+        user?: UserEntity
     }
 }
