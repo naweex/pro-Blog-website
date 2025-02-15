dfb1c29706f0e26de438c35d35e289914ab86e24
navid bahrami <navidbahrami1996@gmail.com>
2025-02-15 19:11:38 +0330

    add google user type

--- src/modules/auth/types/response.ts
+++ src/modules/auth/types/response.ts
@@ -2,3 +2,11 @@ export type AuthResponse = {
     code : string ,
     token : string
 }
+
+export type GoogleUser = {
+    firstName? : string ,
+    lastName? : string ,
+    email : string ,
+    profile_image? : string ,
+    accessToken? : string
+}
